265a456779de3a209db2dc0810c0aeeeb10f1900
Manuel Grundner <m.grundner@delegate.at>
2019-03-25 12:29:42 +0100

    feat: chat skeleton

---
+++ src/views/Chat.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="chat">
+    <h1>Chat</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+  components: {},
+})
+export default class Home extends Vue {}
+</script>
+
+<style lang="sass" scoped>
+
+</style>
